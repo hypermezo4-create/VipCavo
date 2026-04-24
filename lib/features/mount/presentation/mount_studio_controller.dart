dd926f21a222963f23cdd81677656efea8a5f9b5
Mohammed-MEZO <hypermezo4@gmail.com>
2026-04-24 11:02:22 +0300

    Fix Mount Studio analyzer import issues

--- lib/features/mount/presentation/mount_studio_controller.dart
+++ lib/features/mount/presentation/mount_studio_controller.dart
@@ -4,7 +4,7 @@ import 'package:deadzon/features/mount/domain/mount_monet_app.dart';
 import 'package:deadzon/features/mount/domain/mount_profile.dart';
 import 'package:deadzon/features/mount/services/mount_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final mountStudioControllerProvider = ChangeNotifierProvider<MountStudioController>((ref) {
   final controller = MountStudioController(service: MountService());
