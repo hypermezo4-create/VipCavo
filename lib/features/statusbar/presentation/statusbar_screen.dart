3076bcee01b725a87f47b173364754bc5b727eff
Mohammed-MEZO <hypermezo4@gmail.com>
2026-04-27 21:13:26 +0300

    Fix unnecessary underscores lint in statusbar separator builder

--- lib/features/statusbar/presentation/statusbar_screen.dart
+++ lib/features/statusbar/presentation/statusbar_screen.dart
@@ -2479,7 +2479,7 @@ class _BatteryOptionSheet extends StatelessWidget {
               child: ListView.separated(
                 shrinkWrap: true,
                 itemCount: options.length,
-                separatorBuilder: (_, __) => const SizedBox(height: 8),
+                separatorBuilder: (_, _) => const SizedBox(height: 8),
                 itemBuilder: (context, index) {
                   final option = options[index];
                   return InkWell(
